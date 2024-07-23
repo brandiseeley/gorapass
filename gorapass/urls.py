86c356d0527c1a11e475cc31810464cabcae5ec7
Naya Olmer <nayaolmer@gmail.com>
2024-07-23 14:44:04 +0200

    Added API to retrieve list of all stamps

--- gorapass/urls.py
+++ gorapass/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('stamps', views.stamps, name='stamps'),
     path('hikes/<int:hike_id>', views.hike, name='hike'),
     path('populate_stamps_datatable', views.populate_stamps_datatable, name='populate_stamps_datatable'),
     path('populate_hikes_datatable', views.populate_hikes_datatable, name='populate_hikes_datatable'),
