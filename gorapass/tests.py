dbfc32643e5948c656c7f6c5dece2b3140cdd44a
Brandi Seeley <brandi.n.seeley@gmail.com>
2024-07-24 11:58:25 +0800

    First test

--- gorapass/tests.py
+++ gorapass/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import Client, TestCase
 
-# Create your tests here.
+class HikesTestCase(TestCase):
+    def setUp(self):
+        HikesTestCase.client = Client()
+
+    def test_hikes_endpoint(self):
+        """GET requests to the hikes endpoint give us a 200 status"""
+        response = HikesTestCase.client.get('/gorapass/hikes')
+        self.assertEqual(response.status_code, 200)
